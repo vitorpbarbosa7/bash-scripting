c79489359eb85125807eafec90baf577931e136e
vitor <vitorpbarbosa7@gmail.com>
2022-02-20 23:34:55 -0300

    hereDoc

--- append_to_text.sh
+++ append_to_text.sh
@@ -3,4 +3,10 @@
 # to store, use single angle bracket
 
 # to append to existing file, use two angle brackets
+
+: '
+this is a cat command 
+this is a cat command 
+this is a cat command'
+
 cat >> file.txt
