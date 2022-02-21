572c714f8ad575aa6acba3ddbc6dc840a917d386
vitor <vitorpbarbosa7@gmail.com>
2022-02-21 08:48:07 -0300

    case statement

--- andoperator.sh
+++ andoperator.sh
@@ -2,9 +2,9 @@ age=30
 minor=20
 major=40
 
-if [ "$age" -gt "$minor" ] && [ "$age" -lt "$major" ]
+if [[ "$age" -gt "$minor"  ||  "$age" -lt "$major" ]]
 then
-	echo "Age ${age} is between ${minor} and ${major}"
+	echo "Age ${age} is greater than  ${minor} or less than  ${major}"
 else
 	echo "Age less than ${minor} or greater than ${major}"
 fi
